3fa7ee05c223892b7ad251aea962ebf6fa2455ea
tars0x9752 <46079709+tars0x9752@users.noreply.github.com>
2022-08-21 23:22:34 +0900

    add comments

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     templates =
       with builtins;
       let
-        # add prefix "_" to official templates
+        # add prefix "_" to official template names
         prefixed-list = map (name: { name = "_${name}"; value = official-templates.templates."${name}"; }) (attrNames official-templates.templates);
         officials = listToAttrs prefixed-list;
       in
@@ -23,7 +23,7 @@
           path = ./purescript;
           description = "A purescript project template";
         };
-      } // officials;
+      } // officials; # exntends official templates (their names are prefixed with "_")
 
   };
 }
